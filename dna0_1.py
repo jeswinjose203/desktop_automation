8e6b1ba68873e43d99191e5ab58363f20771cb60
Keerthiii098 <keerthibinu2507@gmail.com>
2023-03-24 15:00:21 +0530

    dna0_1

--- dna0_1.py
+++ dna0_1.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import webbrowser
+import subprocess
+import os
 # create a recognizer object
 r = sr.Recognizer()
 text = ""
@@ -15,7 +17,7 @@ with sr.Microphone() as source:
         try:
             # recognize speech using Google Speech Recognition
             text = r.recognize_google(audio)
-            test = '''open chittappan tracker open chittapan tracker open chitapan tracker open Chittagong tracker open set up and tracker  open setup and tracker open sit up and tracker
+            test = '''open chittappan tracker open chittapan tracker open chitapan tracker open tracker open sithappan racket open situp and tracker open sit apan tracker open Chittagong tracker open set up and tracker  open setup and tracker open sit up and tracker
             open situp and rocker 
             bus tracker'''
             text_1_close = '''close tracker close chittapan tracker close chitappan tracker close chittappan tracker close set up and tracker close sit up and tracker
@@ -23,12 +25,21 @@ with sr.Microphone() as source:
             close chittagong tracker close Chitta pandrakar
             '''
             windows = {}
-
+            text_discord = ''' 
+            open discord discord escort listening open discord open the sport pin code open Disco open discord discord open the score open the Scott
+            open scot open Disco viscose open the spot open the sport
+            '''
+            text_telegram = '''
+            open telegram telegram
+            '''
+            text_computer = '''
+            open computer my computer my pc open my computer
+            '''
             print(text)
             if(text in test):
-                link = "https://chittapan-tracker.onrender.com"
+                link = "https://chittapan-tracker.onrender.com" 
                 windows['chittapan'] = webbrowser.open_new_tab(link)
-            test_1 = '''bus track one track bus number one bus truck 1 bus number 1 track bus number 1 Raat bus number 1'''
+            test_1 = '''bus track one track bus number one number ones bus truck 1 bus number 1 track bus number 1 Raat bus number 1'''
             if(text in test_1):
                 link_bus_no_1 = "https://chittapan-tracker.onrender.com/bus_no_1"
                 windows['bus_no_1'] = webbrowser.open_new_tab(link_bus_no_1)
@@ -36,7 +47,12 @@ with sr.Microphone() as source:
             if(text in text_1_close):
                 if 'chittapan' in windows:
                     windows['chittapan'].close()
-
+            if(text in text_telegram):
+                subprocess.Popen("C:\\Users\\Acer\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe")
+            if(text in text_discord):
+                subprocess.Popen("C:\\Users\\Acer\\AppData\\Local\\Discord\\app-1.0.9011\\Discord.exe")
+            if(text in text_computer):
+                subprocess.Popen('explorer /This PC,"::{20D04FE0-3AEA-1069-A2D8-08002B30309D}"')
 
         except sr.UnknownValueError:
             print("Sorry, I could not understand what you said.")
